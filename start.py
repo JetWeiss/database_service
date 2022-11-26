8ae1916303853fa781c98c2aec084e723bdff520
JetWeiss <101409538+JetWeiss@users.noreply.github.com>
2022-11-26 12:53:33 +0300

    Update start.py
    Fixing a bug when deleting rows in the database.

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ from threading import Thread, Event
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 import time
+from sqlalchemy.sql import and_
 
 event = Event()
 
@@ -16,90 +17,111 @@ def PauseOrEnabled(session1, session2, change_b):
     client_gr2 = session2.query(Client_group).filter(Client_group.group_id == tables_info2.group_id).first()
     channel_ch1 = session1.query(Channel).filter(Channel.channel_id == change_b.src_table_name).first()
     channel_ch2 = session2.query(Channel).filter(Channel.channel_id == change_b.src_table_name).first()
-    if (client_ch1.enabled == True and client_ch1.paused == False) and (client_ch2.enabled == True and client_ch2.paused == False)\
-        and (client_gr1.enabled == True and client_gr1.paused == False) and (client_gr2.enabled == True and client_gr2.paused == False) \
-            and (tables_info1.enabled == True and tables_info1.paused == False) and (tables_info2.enabled == True and tables_info2.paused == False) \
-                and (channel_ch1.enabled == True and channel_ch1.paused == False) and (channel_ch2.enabled == True and channel_ch2.paused == False):
+    if (client_ch1.enabled == True and client_ch1.paused == False) \
+            and (client_ch2.enabled == True and client_ch2.paused == False) \
+            and (client_gr1.enabled == True and client_gr1.paused == False) \
+            and (client_gr2.enabled == True and client_gr2.paused == False) \
+            and (tables_info1.enabled == True and tables_info1.paused == False) \
+            and (tables_info2.enabled == True and tables_info2.paused == False) \
+            and (channel_ch1.enabled == True and channel_ch1.paused == False) \
+            and (channel_ch2.enabled == True and channel_ch2.paused == False):
         return True
     else:
         return False
 
+def StatusRecord(batch, status_b, session):
+    batch.batch_status = status_b
+    session.commit()
+
+def MakeChanges(modified_string, batch, Trans_IU, Truns_D, session1, session2):
+    if modified_string.event_type == 'I' or modified_string.event_type == 'U':
+        try:
+            session2.add(Trans_IU)
+            session2.flush()
+            session2.commit()
+            print('I')
+        except (SQLAlchemyError or RuntimeError) as e:
+            batch.error_string = 'Error inserting transaction: {error}'.format(error=e)
+            batch.batch_status = 'E'
+            session1.commit()
+    elif modified_string.event_type == 'D':
+        try:
+            if Truns_D != None:
+                Truns_D.delete()
+            session2.flush()
+            session2.commit()
+            print('D')
+        except (SQLAlchemyError or RuntimeError) as e:
+            batch.error_string = 'Error deleting transaction: {error}'.format(error=e)
+            batch.batch_status = 'E'
+            session1.commit()
+
 def SearchChanges(session1, session2):
     while True:
         event.wait()
-        res = None
-        Trans_in = None
+        new_batch = None
+        Trans_IU = None
+        Truns_D = None
         Change_table = None
         primary_key_t = None
-        while res == None:
-                res = session1.query(Batch).filter(Batch.batch_status == "N").first()
-                if res == None:
-                    time.sleep(10)
-        res.start_process_dt = 'now()'
-        res.batch_status = 'L'
-        session1.commit()
+        primary_key_v = None
+        while new_batch == None:
+            new_batch = session1.query(Batch).filter(Batch.batch_status == "N").first()
+            if new_batch == None:
+                time.sleep(10)
+        new_batch.start_process_dt = 'now()'
+        StatusRecord(new_batch, 'L', session1)
         start_time = datetime.now()
-        for i in range(0, res.batch_record_count):
-            change_b = session1.query(Change).filter(Change.batch_id == res.batch_id).offset(i).first()
-            if change_b.src_table_name == 'transactions':
+        co_batches = 0
+        while co_batches < new_batch.batch_record_count:
+            change_b = None
+            change_b = session1.query(Change).filter(Change.batch_id == new_batch.batch_id).offset(co_batches).limit(1).all()
+            co_batches += 1
+            if change_b[0].src_table_name == 'transactions':
                 Change_table = Transaction
-                primary_key_t = Change_table.transaction_id
-            elif change_b.src_table_name == 'employees':
+                primary_key_t = Change_table.transactionUuid
+                primary_key_v = Change_table.tableVersion
+            elif change_b[0].src_table_name == 'employees':
                 Change_table = Employee
                 primary_key_t = Change_table.insurance_id
-            elif change_b.src_table_name == 'turns':
+            elif change_b[0].src_table_name == 'turns':
                 Change_table = Turn
-                primary_key_t = Change_table.turn_id
-            if change_b.pk_column_values[1] != '2':
-                Trans_up = session1.query(Change_table).filter(primary_key_t == change_b.pk_column_values[0]).first()
-                Trans_in = session2.merge(Trans_up)
-            res.start_send_dt = 'now()'
-            res.batch_status = 'S'
-            session1.commit()
-            while PauseOrEnabled(session1, session2, change_b) == False:
+                primary_key_t = Change_table.turnUuid
+            if change_b[0].event_type != 'D':
+                if change_b[0].src_table_name == 'transactions':
+                    Trans_up = session1.query(Change_table).filter(
+                        and_(primary_key_v == change_b[0].pk_column_values[1],
+                             primary_key_t == change_b[0].pk_column_values[0])).limit(1).all()
+                else:
+                    Trans_up = session1.query(Change_table).filter(
+                        primary_key_t == change_b[0].pk_column_values[0]).limit(1).all()
+                Trans_IU = session2.merge(Trans_up[0])
+            else:
+                if change_b[0].src_table_name == 'transactions':
+                    Truns_D = session2.query(Change_table).filter(
+                        and_(primary_key_v == change_b[0].pk_column_values[1],
+                             primary_key_t == change_b[0].pk_column_values[0]))
+                else:
+                    Truns_D = session2.query(Change_table).filter(
+                        primary_key_t == change_b[0].pk_column_values[0])
+            new_batch.start_send_dt = 'now()'
+            StatusRecord(new_batch, 'S', session1)
+            while PauseOrEnabled(session1, session2, change_b[0]) == False:
                 time.sleep(5)
-            if change_b.event_type == 'I':
-                try:
-                    print('I')
-                    session2.add(Trans_in)
-                    session2.commit()
-                except (SQLAlchemyError or RuntimeError) as e:
-                    res.error_string = 'Error inserting transaction: {error}'.format(error=e)
-                    res.batch_status = 'E'
-                    session1.commit()
-            elif change_b.event_type == 'U':
-                try:
-                    print('U')
-                    Trans_ch = session2.query(Change_table).filter(primary_key_t == change_b.pk_column_values[0]).first()
-                    Trans_ch = Trans_in
-                    session2.commit()
-                except (SQLAlchemyError or RuntimeError) as e:
-                    res.error_string = 'Error updating transaction: {error}'.format(error=e)
-                    res.batch_status = 'E'
-                    session1.commit()
-            elif change_b.event_type == 'D':
-                try:
-                    print('D')
-                    Trans_ch = session2.query(Change_table).filter(primary_key_t == change_b.pk_column_values[0])
-                    Trans_ch.delete()
-                    session2.commit()
-                except (SQLAlchemyError or RuntimeError) as e:
-                    res.error_string = 'Error deleting transaction: {error}'.format(error=e)
-                    res.batch_status = 'E'
-                    session1.commit()
-            if change_b.src_table_name == 'transactions':
-                miss_c = None
-                while miss_c == None:
-                    miss_c = session2.query(Batch).filter(Batch.batch_id > res.batch_id).first()
-                    if miss_c == None:
-                        time.sleep(1)
-                miss_c.batch_status = 'C'
-                session2.commit()
+            MakeChanges(change_b[0], new_batch, Trans_IU, Truns_D, session1, session2)
+            # if change_b.src_table_name == 'transactions':
+            #     miss_c = None
+            #     while miss_c == None:
+            #         miss_c = session2.query(Batch).filter(Batch.batch_id > new_batch.batch_id).first()
+            #         if miss_c == None:
+            #             time.sleep(1)
+            #     miss_c.batch_status = 'C'
+            #     session2.commit()
         end_time = datetime.now() - start_time
-        res.process_time = end_time
-        res.end_process_dt = 'now()'
-        res.batch_status = 'C'
-        session1.commit()
+        new_batch.process_time = end_time
+        new_batch.end_process_dt = 'now()'
+        StatusRecord(new_batch, 'C', session1)
+
 
 if __name__ == "__main__":
     event.clear()
